db4cf52e11f5bc21d6547b04f8b401dc5cf9c99a
Nicola Zambello <nzambello@protonmail.com>
2021-05-17 14:49:41 +0200

    fix(a11y): close date picker losing focus

--- src/rer/ufficiostampa/browser/static/javascripts/search/components/fields/DateField.js
+++ src/rer/ufficiostampa/browser/static/javascripts/search/components/fields/DateField.js
@@ -1,12 +1,14 @@
-import React, { useContext, useState } from 'react';
-import Select from 'react-select';
+import React, { useContext, useState, useEffect } from 'react';
 import { string, shape, arrayOf, func, bool, object } from 'prop-types';
 import { TranslationsContext } from '../../../TranslationsContext';
 import { DateRangePicker } from 'react-dates';
+import moment from 'moment';
 
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 import './DateField.less';
+
+moment.locale('it');
 
 const DateField = ({
   parameter,
@@ -16,6 +18,40 @@ const DateField = ({
 }) => {
   const getTranslationFor = useContext(TranslationsContext);
   const [focusedDateInput, setFocusedDateInput] = useState(null);
+
+  let language = document.querySelector('html').getAttribute('lang');
+  moment.locale(language);
+
+  useEffect(() => {
+    let startDateInput = document.getElementById('start-date-filter');
+
+    if (startDateInput) {
+      let removeStartDateListener = startDateInput.addEventListener(
+        'keydown',
+        e => {
+          if (e.key === 'Tab' && e.shiftKey) setFocusedDateInput(null);
+        },
+      );
+
+      if (removeStartDateListener) return () => removeStartDateListener();
+    }
+  }, []);
+
+  useEffect(() => {
+    let endDateInput = document.getElementById('end-date-filter');
+
+    if (endDateInput) {
+      let removeEndDateListener = endDateInput.addEventListener(
+        'keydown',
+        e => {
+          if (e.key === 'Tab' && !e.shiftKey) setFocusedDateInput(null);
+        },
+      );
+
+      if (removeEndDateListener) return () => removeEndDateListener();
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <label htmlFor={parameter.id}>{parameter.label}</label>
@@ -39,10 +75,12 @@ const DateField = ({
         noBorder={true}
         numberOfMonths={isMobile ? 1 : 2}
         focusedInput={focusedDateInput}
-        onFocusChange={focusedInput => setFocusedDateInput(focusedInput)}
+        onFocusChange={focusedInput => {
+          setFocusedDateInput(focusedInput);
+        }}
         displayFormat="DD/MM/YYYY"
-        hideKeyboardShortcutsPanel={true}
-        showClearDates={true}
+        hideKeyboardShortcutsPanel
+        showClearDates
         minimumNights={0}
         isOutsideRange={() => {}}
       />
