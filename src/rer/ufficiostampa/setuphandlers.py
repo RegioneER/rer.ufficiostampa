2fa8a6f21e5fa882c88921149650bd6eb0407515
Mauro Amico <mauro.amico@gmail.com>
2024-07-25 18:47:11 +0200

    add behavior iocomune

--- src/rer/ufficiostampa/setuphandlers.py
+++ src/rer/ufficiostampa/setuphandlers.py
@@ -1,5 +1,23 @@
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from plone import api
+from Products.CMFPlone.utils import get_installer
+Crom plone.dexterity.interfaces import IDexterityFTI
+from zope.component import getUtility
+
+
+def set_behavior(fti_id, name, value):
+    """Set a behavior on a FTI
+        if value is True, add the behavior, otherwise remove it
+     """
+    # add or remove the behavior based on the value from the form
+    fti = getUtility(IDexterityFTI, name=fti_id)
+    behaviors = list(fti.behaviors)
+    if value and name not in behaviors:
+        behaviors.append(name)
+    elif not value and name in behaviors:
+        behaviors.remove(name)
+    fti.behaviors = tuple(behaviors)
 
 
 @implementer(INonInstallable)
@@ -13,7 +31,14 @@ class HiddenProfiles:
 
 def post_install(context):
     """Post install script"""
-
+    # Do something during the installation of this package
+    installer = get_installer(context)
+    if installer.is_product_installed("design.plone.contenttypes"):
+        set_behavior("ComunicatoStampa", "design.plone.contenttypes.behavior.argomenti_news", True)
+        set_behavior("ComunicatoStampa", "design.plone.contenttypes.behavior.news_base", True)
+        set_behavior("ComunicatoStampa", "plone.basic", True)
+        set_behavior("ComunicatoStampa", "rer.ufficiostampa.basic", False)
+        set_behavior("ComunicatoStampa", "rer.ufficiostampa.legislature", False)
 
 def uninstall(context):
     """Uninstall script"""
