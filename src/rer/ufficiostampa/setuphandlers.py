e129a0e425daa172a6155800e5a318b1dadcd4db
Mauro Amico <mauro.amico@gmail.com>
2024-07-25 18:48:14 +0200

    black

--- src/rer/ufficiostampa/setuphandlers.py
+++ src/rer/ufficiostampa/setuphandlers.py
@@ -1,15 +1,15 @@
-from Products.CMFPlone.interfaces import INonInstallable
-from zope.interface import implementer
 from plone import api
+from plone.dexterity.interfaces import IDexterityFTI
+from Products.CMFPlone.interfaces import INonInstallable
 from Products.CMFPlone.utils import get_installer
-Crom plone.dexterity.interfaces import IDexterityFTI
 from zope.component import getUtility
+from zope.interface import implementer
 
 
 def set_behavior(fti_id, name, value):
     """Set a behavior on a FTI
-        if value is True, add the behavior, otherwise remove it
-     """
+    if value is True, add the behavior, otherwise remove it
+    """
     # add or remove the behavior based on the value from the form
     fti = getUtility(IDexterityFTI, name=fti_id)
     behaviors = list(fti.behaviors)
@@ -34,11 +34,18 @@ def post_install(context):
     # Do something during the installation of this package
     installer = get_installer(context)
     if installer.is_product_installed("design.plone.contenttypes"):
-        set_behavior("ComunicatoStampa", "design.plone.contenttypes.behavior.argomenti_news", True)
-        set_behavior("ComunicatoStampa", "design.plone.contenttypes.behavior.news_base", True)
+        set_behavior(
+            "ComunicatoStampa",
+            "design.plone.contenttypes.behavior.argomenti_news",
+            True,
+        )
+        set_behavior(
+            "ComunicatoStampa", "design.plone.contenttypes.behavior.news_base", True
+        )
         set_behavior("ComunicatoStampa", "plone.basic", True)
         set_behavior("ComunicatoStampa", "rer.ufficiostampa.basic", False)
         set_behavior("ComunicatoStampa", "rer.ufficiostampa.legislature", False)
+
 
 def uninstall(context):
     """Uninstall script"""
