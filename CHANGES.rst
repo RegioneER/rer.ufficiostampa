f37f22063535c589048a5708bdea0a72b22c9d04
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-09-17 14:58:55 +0200

    Preparing release 3.0.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-3.0.0 (unreleased)
+3.0.0 (2025-09-17)
 ------------------
 
 - Revert to RER functionalities.
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="rer.ufficiostampa",
-    version="3.0.0.dev0",
+    version="3.0.0",
     description="Policy for Ufficio Stampa",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
